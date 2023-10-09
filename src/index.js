47eee34f70bed012c5af2a2a25e36f265202ecb6
aamnatayyab6 <aamnatayyab6@yahoo.com>
2023-10-09 05:08:23 +0200

    enabling cors policy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(express.json());
@@ -7,6 +9,7 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const contactRoutes = require('./routes/contactRoutes');
+app.use(cors());
 
 // API routes
 app.use('/api/contacts', contactRoutes);
